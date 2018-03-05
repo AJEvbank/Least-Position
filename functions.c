8c888894923e619fa8a3b668817c36b53be1e4ba
Cbellando <caleb.bellando@ttu.edu>
2018-03-05 16:49:09 -0600

    Update functions.c

--- functions.c
+++ functions.c
@@ -4,6 +4,38 @@
 
 #include "main.h"
 
-
+Void f(int SOURCE, int n, int **edge, int *dist){ 
+  int i, j, count, *found; 
+  found = (int *) calloc( n, sizeof(int) ); 
+  for(i=0 ; i<n; i++) { 
+    found[i] = 0; 
+    dist [i] = edge[SOURCE] [i]; 
+} 
+  
+  found[SOURCE] = 1; 
+  count = 1; 
+  while(count < n ) { 
+  j = choose(dist, n, found); 
+  found[j] = 1 ; 
+  count++; 
+  for(i=0; i<n; i++) { 
+    if ( !(found[i]) ) 
+    dist[i] = min(dist[i],dist[j]+edge[j] [i]; 
+} 
+      } 
+free (found); 
+} 
+ 
+int choose(int *dist, int n int *found){ 
+int i, tmp, least = INFTY, leastPosition; 
+for(i=0; i<n; i++) { 
+tmp = dist[i]; 
+if( (!found[i]) && (tmp < least) ) { 
+least =tmp; 
+leastPosition = I; 
+} 
+} 
+teturn leastPosition; 
+} 
 
 #endif
